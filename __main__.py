97840d722d5215181640dbc6806b2f4f74de9e62
Nikita Bulaev <Nikita.Bulaev@mir.dev>
2025-05-19 13:41:49 +0400

    Update __main__

--- __main__.py
+++ __main__.py
@@ -1,4 +1,10 @@
+import time
+
 from main import main
+from rich import print
 
 if __name__ == "__main__":
+    start = time.time()
     main()
+    end = time.time()
+    print(f"[green]INFO[/green] Total flattering time: {(end - start):.2f} seconds")
